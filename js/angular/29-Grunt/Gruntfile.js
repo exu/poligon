98efc452298faa3e5a21df1fc47a33ef8cac044c
Jacek Wysocki <jacek.wysocki@gmail.com>
2013-12-16 12:21:48 +0100

    some basic tasks

--- js/angular/29-Grunt/Gruntfile.js
+++ js/angular/29-Grunt/Gruntfile.js
@@ -2,6 +2,7 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+
     concat: {
       site: {
         src: ['src/app.js', 'src/lib.js'],
@@ -11,14 +12,35 @@ module.exports = function(grunt) {
         src: ['src/lib.js', 'src/app.js'],
         dest: 'dest/details.js'
       }
+    },
+
+    jshint: {
+      files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+      options: {
+        // options here to override JSHint defaults
+        globals: {
+          jQuery: true,
+          console: true,
+          module: true,
+          document: true
+        }
+      }
+    },
+
+    watch: {
+      files: ['<%= jshint.files %>'],
+      tasks: ['jshint']
     }
+
   });
 
-
   grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  grunt.registerTask('test', ['jshint', 'nodeunit']);
 
   grunt.registerTask('default', 'Log some stuff.', function() {
     grunt.log.write('Logging some stuff...').ok();
   });
-
 };
