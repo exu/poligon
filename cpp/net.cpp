331c7af4f77616c608d08a4be595ff0e9b724793
Jacek Wysocki <jacek.wysocki@gmail.com>
2014-04-23 10:13:21 +0200

    poligon

---
+++ cpp/net.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    using namespace std;
+
+    string something;
+
+    codut << "AAAAAAAAA";
+    cin >> something;
+
+    return 0;
+}
