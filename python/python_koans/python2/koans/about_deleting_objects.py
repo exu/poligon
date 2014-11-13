689b3286800d90ffe82dde952947b73c8ac23b44
Jacek Wysocki <jacek.wysocki@gmail.com>
2014-11-13 06:14:53 +0100

    starting proxy project class :)

--- python/python_koans/python2/koans/about_deleting_objects.py
+++ python/python_koans/python2/koans/about_deleting_objects.py
@@ -9,9 +9,9 @@ class AboutDeletingObjects(Koan):
         lottery_nums = [4, 8, 15, 16, 23, 42]
         del lottery_nums[1]
         del lottery_nums[2:4]
-        
-        self.assertEqual(__, lottery_nums)
-        
+
+        self.assertEqual([4, 15, 42], lottery_nums)
+
     def test_del_can_remove_entire_lists(self):
         lottery_nums = [4, 8, 15, 16, 23, 42]
         del lottery_nums
@@ -19,29 +19,29 @@ class AboutDeletingObjects(Koan):
             win = lottery_nums
         except Exception as e:
             pass
-        self.assertMatch(__, e[0])
-        
+        self.assertMatch("referenced before assignment", e[0])
+
     # --------------------------------------------------------------------
-        
+
     class ClosingSale(object):
         def __init__(self):
             self.hamsters = 7
             self.zebras = 84
-            
+
         def cameras(self):
             return 34
-        
+
         def toilet_brushes(self):
             return 48
-        
+
         def jellies(self):
             return 5
-        
+
     def test_del_can_remove_attributes(self):
         crazy_discounts = self.ClosingSale()
         del self.ClosingSale.toilet_brushes
         del crazy_discounts.hamsters
-        
+
         try:
             still_available = crazy_discounts.toilet_brushes()
         except AttributeError as e:
@@ -51,16 +51,16 @@ class AboutDeletingObjects(Koan):
             still_available = crazy_discounts.hamsters
         except AttributeError as e:
             err_msg2 = e.args[0]
-        
-        self.assertMatch(__, err_msg1)
-        self.assertMatch(__, err_msg2)
+
+        self.assertMatch("has no attribute", err_msg1)
+        self.assertMatch("has no attribute", err_msg2)
 
     # --------------------------------------------------------------------
 
     class ClintEastwood(object):
         def __init__(self):
             self._name = None
-            
+
         def get_name(self):
             try:
                 return self._name
@@ -69,27 +69,27 @@ class AboutDeletingObjects(Koan):
 
         def set_name(self, name):
             self._name = name
-            
+
         def del_name(self):
             del self._name
-            
+
         name = property(get_name, set_name, del_name, \
             "Mr Eastwood's current alias")
-            
+
     def test_del_works_with_properties(self):
         cowboy = self.ClintEastwood()
         cowboy.name = 'Senor Ninguno'
         self.assertEqual('Senor Ninguno', cowboy.name)
 
         del cowboy.name
-        self.assertEqual(__, cowboy.name)
-        
+        self.assertEqual("The man with no name", cowboy.name)
+
     # --------------------------------------------------------------------
 
     class Prisoner(object):
         def __init__(self):
             self._name = None
-        
+
         @property
         def name(self):
             return self._name
@@ -97,26 +97,26 @@ class AboutDeletingObjects(Koan):
         @name.setter
         def name(self, name):
             self._name = name
-            
+
         @name.deleter
         def name(self):
             self._name = 'Number Six'
-                        
+
     def test_another_way_to_make_a_deletable_property(self):
         citizen = self.Prisoner()
         citizen.name = "Patrick"
         self.assertEqual('Patrick', citizen.name)
 
         del citizen.name
-        self.assertEqual(__, citizen.name)
+        self.assertEqual("Number Six", citizen.name)
 
     # --------------------------------------------------------------------
-        
+
     class MoreOrganisedClosingSale(ClosingSale):
         def __init__(self):
             self.last_deletion = None
             super(AboutDeletingObjects.ClosingSale, self).__init__()
-        
+
         def __delattr__(self, attr_name):
             self.last_deletion = attr_name
 
@@ -124,4 +124,4 @@ class AboutDeletingObjects(Koan):
         sale = self.MoreOrganisedClosingSale()
         self.assertEqual(5, sale.jellies())
         del sale.jellies
-        self.assertEqual(__, sale.last_deletion)
+        self.assertEqual("jellies", sale.last_deletion)
